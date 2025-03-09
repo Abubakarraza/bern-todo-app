2f5b86811e1c80a51917dc9440240d3203b7c25e
Abubakarraza <abubakarraza64@gmail.com>
2025-03-10 02:51:27 +0500

    Add functions to retrieve, view, and delete tasks in Todo contract

--- smart-contract/contracts/Todo.sol
+++ smart-contract/contracts/Todo.sol
@@ -35,4 +35,25 @@ contract Todo {
     ) public {
         tasks[_taskId] = Task(_taskId, _taskName, _taskDate);
     }
+
+    function allTask() public view returns (Task[] memory) {
+        Task[] memory taskList = new Task[](taskId - 1);
+        for (uint256 i = 0; i < taskId - 1; i++) {
+            taskList[i] = tasks[i + 1];
+        }
+        return taskList;
+    }
+
+    function viewTask(uint256 _taskId)
+        public
+        view
+        checkId(_taskId)
+        returns (Task memory)
+    {
+        return tasks[_taskId];
+    }
+
+    function deleteTask(uint256 _taskId) public checkId(_taskId) {
+        delete tasks[_taskId];
+    }
 }
